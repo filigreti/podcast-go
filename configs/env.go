df95aa304778558ea8b36cdca76b3f32c1a0ca08
filigreti <mac@macs-MacBook-Pro.local>
2023-12-21 13:46:13 +0100

    wip

--- configs/env.go
+++ configs/env.go
@@ -8,6 +8,12 @@ import (
 )
 
 func GetEnv(key string) string {
+	if os.Getenv("ENVIRONMENT") == "production" {
+		// In production, directly use environment variables without loading from file
+		return os.Getenv(key)
+	}
+
+	// For non-production environments, attempt to load from the .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
