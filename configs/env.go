f79e3e2d53fe10677429a0bd6b53dad4b5abaa20
filigreti <mac@macs-MacBook-Pro.local>
2023-12-21 13:27:32 +0100

    error handling env

--- configs/env.go
+++ configs/env.go
@@ -7,11 +7,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetEnv(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+func GetEnv(key string, defaultValue string) string {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found or cannot be loaded. Using default values.")
+		return defaultValue
 	}
 
-	return os.Getenv(key)
+	value := os.Getenv(key)
+	if value == "" {
+		log.Printf("Warning: Environment variable %s is not set. Using default value.\n", key)
+		return defaultValue
+	}
+
+	return value
 }
