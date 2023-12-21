bc457dd3f810da02ba40fb338d4c1c5ce6ddc107
filigreti <mac@macs-MacBook-Pro.local>
2023-12-21 14:28:44 +0100

    wip

--- routes/user_route.go
+++ routes/user_route.go
@@ -1,12 +1,17 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/filigreti/podcast-backend/controllers"
 	"github.com/filigreti/podcast-backend/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func UserRoute(e *echo.Echo) {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Great!, Nothing to see here")
+	})
 	e.POST("/register", controllers.CreateUser)
 	e.POST("/login", controllers.UserLogin)
 	e.GET("/user", controllers.GetUser, middleware.TokenMiddleware)
